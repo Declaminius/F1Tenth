fe17a3b8f53c4948c7e85ca46eda311ba935396d
Ubuntu <schagerflorian@gmail.com>
2023-03-24 08:39:01 -0400

    Removed build and devel

--- catkin_ws/build/safety_node1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/F1Tenth/catkin_ws/src/safety_node1
-# Build directory: /home/ubuntu/F1Tenth/catkin_ws/build/safety_node1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
