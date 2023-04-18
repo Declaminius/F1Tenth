89aab83bac286f9000ad5f2f9a12623b7becdc11
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-04-18 17:07:18 -0400

    .

--- catkin_ws/src/wall_follow/scripts/wall_follow.py
+++ catkin_ws/src/wall_follow/scripts/wall_follow.py
@@ -12,13 +12,13 @@ from nav_msgs.msg import Odometry
 from std_msgs.msg import Float32
 
 
-from dynamic_reconfigure.server import Server
-from wall_follow2.cfg import wall_follow2Config
+# from dynamic_reconfigure.server import Server
+# from wall_follow2.cfg import wall_follow2Config
 
 #PID CONTROL PARAMS
 
-thetas = [lol / 180 *np.pi for lol in [35,45,60,70]]
-L = 1
+thetas = [angle / 180 *np.pi for angle in [35,45,60,70]]
+L = 2
 
 servo_offset = 0.0
 prev_error = 0.0 
@@ -42,26 +42,24 @@ class WallFollow:
         lidarscan_topic = '/scan'
         drive_topic = '/nav'
         
-        self.kp = 1.4
+        self.kp = 14
         self.kd = 0.09
         self.ki = 0
         self.actual_speed = 0
-        self.desired_speed = 1.2
 
         self.lidar_sub = rospy.Subscriber("/scan", LaserScan, self.lidar_callback)
         self.odom_sub = rospy.Subscriber("odom", Odometry, self.odom_callback)
-        
-        self.desired_speed_pub = rospy.Publisher("/desired_speed_topic", Float32, queue_size = 1000)
+
         self.actual_speed_pub = rospy.Publisher("/actual_speed_topic", Float32, queue_size = 1000)
         self.steering_angle_pub = rospy.Publisher("/steering_angle_topic", Float32, queue_size = 1000)
         self.left_dist_pub = rospy.Publisher("/left_dist_topic", Float32, queue_size = 1000)
+        self.desired_dist_pub = rospy.Publisher("/desired_dist_topic", Float32, queue_size = 1000)
         self.drive_pub = rospy.Publisher("/nav", AckermannDriveStamped, queue_size = 1000)
 
     def getRange(self, scan_msg, angle):
         # data: single message from topic /scan
         # angle: between -45 to 225 degrees, where 0 degrees is directly to the right
         # Outputs length in meters to object with angle in lidar scan field of view
-        # TODO: make sure to take care of nans etc.
 
         angle_degrees = angle * 180 / np.pi
         position = (angle_degrees + 45) / 270
@@ -75,17 +73,22 @@ class WallFollow:
         global prev_time
         global time_steps
         global speed
-        threshold = 1
+        threshold = 2
         
         integral += (error - integral)/(time_steps+1)
         
         derivative = (error - prev_error)/(time - prev_time)
         angle = self.kp*error + self.ki*integral + self.kd*derivative
         
-        speed = 100*self.desired_speed/abs(angle*180/np.pi)
+        if 0 <= abs(angle) < 10:
+            speed = 1.5
+        elif 10 <= abs(angle) < 20:
+            speed = 1
+        else:
+            speed = 0.5
         
         if self.getRange(scan_msg, np.pi / 2) < threshold*self.actual_speed:
-            speed = 1*self.desired_speed
+            speed = 0.5
 
         prev_time = time
         prev_error = error
@@ -94,19 +97,17 @@ class WallFollow:
         drive_msg = AckermannDriveStamped()
         drive_msg.header.stamp = rospy.Time.now()
         drive_msg.header.frame_id = "laser"
-        drive_msg.drive.steering_angle = angle
+        drive_msg.drive.steering_angle = angle/180*np.pi
         drive_msg.drive.speed = speed
         
         self.drive_pub.publish(drive_msg)
-        self.desired_speed_pub.publish(speed)
-        self.steering_angle_pub.publish(angle*180/np.pi)
+        self.steering_angle_pub.publish(angle)
         
 
     def followLeft(self, scan_msg, leftDist):
         #Follow left wall as per the algorithm 
         global thetas
         global L
-        global speed
 
         error_average = 0
         for theta in thetas:
@@ -124,27 +125,33 @@ class WallFollow:
             # print(f"{next_distance=}")
             # print(f"{theta=}")
         error_average /= len(thetas)
-        self.left_dist_pub.publish(error_average)        
+        self.left_dist_pub.publish(error_average + leftDist)
+        self.desired_dist_pub.publish(leftDist)       
         return error_average
         
     def followRight(self, scan_msg, rightDist):
-        #Follow left wall as per the algorithm 
-        # TODO: averaging over thetas
-       
-        global theta
+        #Follow right wall as per the algorithm 
+        global thetas
         global L
-        
-        a = self.getRange(scan_msg, theta)
-        b = self.getRange(scan_msg, 0)
- 
-        alpha = np.arctan((a*np.cos(theta) - b)/(a*np.sin(theta)))
-        D_t = b * np.cos(alpha)
-        
-        next_distance = D_t + L*np.sin(alpha)
-        
-        error = rightDist - next_distance
-       
-        return error
+
+        error_average = 0
+        for theta in thetas:
+
+            a = self.getRange(scan_msg, theta)
+            b = self.getRange(scan_msg, 0)
+    
+            alpha = np.arctan((a*np.cos(theta) - b)/(a*np.sin(theta)))
+            
+            D_t = b * np.cos(alpha)
+            
+            next_distance = D_t + L*np.sin(np.pi - alpha)
+            
+            error_average += next_distance - leftDist
+            # print(f"{next_distance=}")
+            # print(f"{theta=}")
+        error_average /= len(thetas)
+        self.left_dist_pub.publish(error_average + leftDist)        
+        return error_average
        
 
     def lidar_callback(self, scan_msg):
@@ -171,7 +178,7 @@ def main(args):
     rospy.init_node("wall_follow", anonymous=True)
     wf = WallFollow()
     rospy.sleep(0.1)
-    srv = Server(wall_follow2Config, wf.dyn_callback)
+    # srv = Server(wall_follow2Config, wf.dyn_callback)
     rospy.spin()
 
 if __name__=='__main__':
