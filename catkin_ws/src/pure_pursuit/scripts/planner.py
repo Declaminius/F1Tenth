6e869a70cc34380b9b055b3649cdfdcd2be8f129
Declaminius <schagerflorian@gmail.com>
2023-05-26 13:44:50 +0200

    shortest path

--- catkin_ws/src/pure_pursuit/scripts/planner.py
+++ catkin_ws/src/pure_pursuit/scripts/planner.py
@@ -9,7 +9,7 @@ import networkx
 import rospy
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
-from nav_msgs.msg import Odometry, OccupancyGrid
+from nav_msgs.msg import Odometry, OccupancyGrid, Path
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import PoseStamped
 
@@ -30,6 +30,7 @@ class PathGenerator:
 
         self.drive_pub = rospy.Publisher("/nav", AckermannDriveStamped, queue_size = 1000)
         self.marker_pub = rospy.Publisher("/visualization_marker", Marker, queue_size = 1000)
+        self.path_pub = rospy.Publisher('/path', Path, latch=True, queue_size=10)
 
         # Racecar state
         self.velocity = 0
@@ -105,22 +106,28 @@ class PathGenerator:
     def odom_callback(self, odom_msg):
         self.velocity = odom_msg.twist.twist.linear.x
     
-    def preprocess_map(self, map_msg):
+    def preprocess_map(self, map_msg, occupancy_treshhold = 10, scale = 3):
         """
         Converts the map_msg.data array into a 2D numpy array.
         Current assumption: The map is centered at (0,0)
-        WARNING: map_msg.info.origin represents the lower-left pixel of the map, not the starting position of the robot!
+        WARNING: map_msg.info.origin represents the lower-right pixel of the map, not the starting position of the robot!
         Shouldn't we use /gt_pose messages to determine the inital pose of the robot, or can we assume it to always be at (0,0)?
         """
 
-        map_width = map_msg.info.width
-        map_height = map_msg.info.height
-        map_resolution = map_msg.info.resolution
+        self.map_width = int(np.ceil(map_msg.info.width/scale))
+        self.map_height = int(np.ceil(map_msg.info.height/scale))
+        self.map_res = map_msg.info.resolution*scale
         
-        map_binary = np.array(map_msg.data).reshape((map_height, map_width))
+        map_data = np.array(map_msg.data).reshape((map_msg.info.width, map_msg.info.height)).T
+        map_data = skimage.measure.block_reduce(map_data, (scale,scale), np.min)
+        # map_data = np.rot90(np.flip(map_data, 0), 1)
+        # Set unknown values to be occupied
+        map_data[map_data == - 1] = 100
+        map_binary = (map_data < occupancy_treshhold).astype(int)
 
         # TODO: Maybe replace hardcoded initial position?
         starting_point = self.convert_position_to_grid_cell(0, 0)
+        print(starting_point)
 
         return map_binary, starting_point
     
@@ -130,42 +137,41 @@ class PathGenerator:
         left_end = y
         right_end = y
 
-        while driveable_area[x,right_end] == 1:
+        while driveable_area[x, right_end] == 1:
             right_end += 1
 
-        while driveable_area[x,left_end] == 1:
+        while driveable_area[x, left_end] == 1:
             left_end -= 1  
 
         rospy.loginfo(f"{right_end=}, {left_end=}")
         return (x, left_end), (x, right_end)
 
     
-    def erode_map(self, map_binary):
+    def erode_map(self, driveable_area):
+        skimage.io.imsave('~/F1Tenth/maps/driveable_area.png', skimage.img_as_ubyte(255*driveable_area), check_contrast=False)
 
-        map_image = np.rot90(np.flip(map_binary, 0), 1)
-        skimage.io.imsave('~/F1Tenth/maps/map_binary.png', skimage.img_as_ubyte(255*map_image), check_contrast=False)
-
-        radius = 10
+        radius = 3
 
         tic = time.time()
-        eroded_map = skimage.morphology.binary_erosion(map_binary, footprint = np.ones((2*radius,2*radius)))
+        eroded_map = skimage.morphology.binary_erosion(driveable_area, footprint = np.ones((2*radius,2*radius)))
         toc = time.time()
 
         rospy.loginfo(f"Time for binary erosion: {toc - tic}")
 
-        map_image = np.rot90(np.flip(eroded_map, 0), 1)
+        map_image = np.flip(np.flip(eroded_map, 1), 0)
 
         skimage.io.imsave('~/F1Tenth/maps/eroded_map.png', skimage.img_as_ubyte(map_image), check_contrast=False)
         print("Saved map")
 
         return eroded_map
     
-    def shortest_path(self, safe_area, finish_line_start, finish_line_end):
+    def shortest_path(self, map_msg, safe_area, finish_line_start, finish_line_end):
         "Use a simple breadth-first search"
         x = finish_line_start[0]
         visited = []
+        finish_line = [(x,y) for y in range(finish_line_start[1], finish_line_end[1] + 1)]
         queue = [(x-1,y) for y in range(finish_line_start[1], finish_line_end[1] + 1)]
-        previous_node = {}
+        previous_node = {(x-1,y): (x,y) for y in range(finish_line_start[1], finish_line_end[1] + 1)}
         path = []
         pos_x, pos_y = self.convert_grid_cell_to_position(finish_line_start[0], finish_line_start[1])
         self.visualize_point(pos_x, pos_y)
@@ -173,28 +179,43 @@ class PathGenerator:
         i = 0
         while queue != []:
             i += 1
-            x,y = queue.pop()
-            if i % 10 == 0:
-                pos_x, pos_y = self.convert_grid_cell_to_position(x,y)
-                
-            if x == finish_line_start[0] and finish_line_start[1] <= y <= finish_line_end[1]:
-                path.append((x,y))
-                break
-    
+            x,y = queue.pop(0)
             visited.append((x,y))
-            for new_x, new_y in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
-                if (new_x, new_y) not in visited and safe_area[new_x, new_y] == 1:
+
+            if i % 100 == 0:
+                print(i)
+                pos_x, pos_y = self.convert_grid_cell_to_position(x,y)
+                self.visualize_point(pos_x,pos_y)
+            if (x,y) in finish_line:
+                path.append((x,y))
+                print("Finish line")
+                break
+
+            if (x+1,y) in finish_line:
+                new_x = x - 1
+                new_y = y
+                if ((new_x, new_y) not in visited) and safe_area[new_x, new_y] == 1:
                     queue.append((new_x,new_y))
-                    visited.append((x,y))
+                    visited.append((new_x,new_y))
                     previous_node[(new_x,new_y)] = (x,y)
+            else:
+                for new_x, new_y in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
+                    if ((new_x, new_y) not in visited) and safe_area[new_x, new_y] == 1:
+                        queue.append((new_x,new_y))
+                        visited.append((new_x,new_y))
+                        previous_node[(new_x,new_y)] = (x,y)
         
-        print(len(previous_node.keys()))
         node = previous_node[path[0]]
         while node not in finish_line:
             node = previous_node[node]
-            path.append(node)
+            pos_x, pos_y = self.convert_grid_cell_to_position(node[0],node[1])
+            cur_pose = PoseStamped()
+            cur_pose.header = map_msg.header
+            cur_pose.pose.position.x = pos_x
+            cur_pose.pose.position.y = pos_y
+            cur_pose.pose.position.z = 0
+            self.path.poses.append(cur_pose)
 
-        return path
             
 
     
@@ -215,52 +236,61 @@ class PathGenerator:
 
 
 
-    def fill4(self, x, y, occupancy_treshhold = 10):
-        "Source: https://wikipedia.org/wiki/Floodfill"
+    def fill4(self, map_binary, x, y):
+        """Source: https://wikipedia.org/wiki/Floodfill
+        0 is occupied
+        1 is free space
+        2 is driveable area
+        """
 
         stack = []
         stack.append((x, y))
         while stack != []:
             (x, y) = stack.pop()
-            if self.map_binary[x,y] != -1 and self.map_binary[x,y] <= occupancy_treshhold:
-                self.map_binary[x,y] = 1000
-                if y + 1 < self.map_width:
+            if map_binary[x,y] == 1:
+                map_binary[x,y] = 2
+                if y + 1 < self.map_height:
                     stack.append((x, y + 1))
                 if y - 1 >= 0:
                     stack.append((x, y - 1))
-                if x + 1 < self.map_height:
+                if x + 1 < self.map_width:
                     stack.append((x + 1, y))
                 if x - 1 >= 0:
                     stack.append((x - 1, y))
         
-        return np.where(self.map_binary == 1000, 1, 0)
+        return map_binary == 2
 
     
     def map_callback(self, map_msg):
         rospy.loginfo(f"Map Header: {map_msg.header}")
         rospy.loginfo(f"Map info: {map_msg.info}")
 
-        self.map_width = map_msg.info.width
-        self.map_height = map_msg.info.height
-        self.map_res = map_msg.info.resolution
+        self.path = Path()
 
-        self.map_binary, starting_point = self.preprocess_map(map_msg)
+        map_binary, starting_point = self.preprocess_map(map_msg)
+        rospy.loginfo(f"number of free grid cells: {np.sum(map_binary)}")
 
         tic = time.time()
-        driveable_area = self.fill4(starting_point[0], starting_point[1])
+        driveable_area = self.fill4(map_binary, starting_point[0], starting_point[1])
         toc = time.time()
         rospy.loginfo(f"Time for FloodFill: {toc - tic}")
+        rospy.loginfo(f"number of driveable grid cells: {np.sum(driveable_area)}")
 
+    
         finish_line_start, finish_line_end = self.calculate_finish_line(driveable_area, starting_point)
 
+        
         safe_area = self.erode_map(driveable_area)
-        shortest_path = self.shortest_path(safe_area, finish_line_start, finish_line_end)
 
-        for i in range(10):
-            rospy.loginfo(shortest_path[i])
-
+        rospy.loginfo(f"number of safe grid cells: {np.sum(safe_area)}")
+        tic = time.time()
+        shortest_path = self.shortest_path(map_msg, safe_area, finish_line_start, finish_line_end)
+        toc = time.time()
+        rospy.loginfo(f"Time for shortest path: {toc - tic}")
 
         
+        self.path.header = map_msg.header
+        self.path_pub.publish(self.path)
 
 
 
