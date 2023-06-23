439f639bfd3e6bb3101de7429462ca260e0b7be3
Larisa Clement <larisaclement@yahoo.com>
2023-06-23 12:12:22 +0200

    Added test yaml for costmap pkg

--- catkin_ws/src/vehicle/scripts/vehicle.py
+++ catkin_ws/src/vehicle/scripts/vehicle.py
@@ -84,7 +84,7 @@ class Vehicle:
         self.pure_pursuit_drive = rospy.Subscriber(pure_pursuit_topic, AckermannDriveStamped, self.pure_pursuit_callback, queue_size=1)
         self.reactive_drive = rospy.Subscriber(reactive_topic, AckermannDriveStamped, self.reactive_callback, queue_size=1)
         self.pure_pursuit_start_index_sub = rospy.Subscriber('/start_pose_index', Int32, self.pure_pursuit_start_index_callback, queue_size=1)
-        self.pp_l_sub = rospy.Subscriber('/lookahead_dist', Float32, self.pure_pursuit_L_callback, queue_size=1)
+        # self.pp_l_sub = rospy.Subscriber('/lookahead_dist', Float32, self.pure_pursuit_L_callback, queue_size=1)
 
         self.scans_pub = rospy.Publisher("/cartesian_scans", Marker, queue_size = 1)
         self.obstacle_marker_pub = rospy.Publisher("/obstacle", Marker, queue_size = 1000)
@@ -104,6 +104,7 @@ class Vehicle:
 
         self.ttc = 1000
         self.velocity = 0.
+        self.yaw = 0.
         self.steering_angle = 0.
         self.map = None
         self.map_matrix = np.matrix([[]])
@@ -111,12 +112,13 @@ class Vehicle:
         rospy.Timer(rospy.Duration(1.0/2.0), self.log_drive_mode)
 
         self.pp_start_index = Int32(0)
-        self.L = 0.5
+        self.L = 1.
 
         self.obstacles_update_sub = rospy.Subscriber(obstacles_topic, OccupancyGridUpdate, self.obstacles_update_callback, queue_size=1)
         self.path_sub = rospy.Subscriber(path_topic, Path, self.path_callback, queue_size=1)
         self.lidar_sub = rospy.Subscriber(lidarscan_topic, LaserScan, self.lidar_callback, queue_size=1)
         self.odom_sub = rospy.Subscriber(odom_topic, Odometry, self.odom_callback, queue_size=1)
+        # self.map_sub = rospy.Subscriber("/map", OccupancyGrid, self.map_callback, queue_size=1)
 
         self.tf_buffer = tf2_ros.Buffer(rospy.Duration(1.0))  # tf buffer length
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
@@ -152,8 +154,24 @@ class Vehicle:
     def pure_pursuit_start_index_callback(self, index):
         self.pp_start_index = index
 
+        # find goal point on path at lookahead_distance L
+        dist = 0
+        i = self.pp_start_index.data + 1
+        poses = np.array([(p.pose.position.x, p.pose.position.y) for p in self.path.poses])
+        ground_pos = self.path.poses[self.pp_start_index.data]
+        ground_pose = (ground_pos.pose.position.x, ground_pos.pose.position.y)
+        while i < len(poses):
+            dist = np.linalg.norm(poses[i] - ground_pose)
+            if dist > self.L:
+                break
+            i += 1
+            # Start a new lap
+            if self.multilap and i == len(poses):
+                i = 0
+        self.goal = poses[i - 1]    
+
     def pure_pursuit_L_callback(self, L):
-        self.L = L.data * 1.8
+        self.L = L.data * 0.5 + 0.8
 
     def pure_pursuit_callback(self, drive):
         self.pp_drive_msg = drive
@@ -178,6 +196,20 @@ class Vehicle:
             self.drive_mode = 'FOLLOW THE GAP'
             self.drive_pub.publish(drive_msg)
 
+    def map_callback(self, data):
+        self.map = data
+        self.map_stamp = data.header.stamp
+        self.map_matrix = np.array(data.data).reshape((data.info.height, data.info.width))
+        # buff = BytesIO()
+        # data.serialize(buff)
+        # serialized_bytes = buff.getvalue()
+
+        # # if self.map_file is not None:
+        # rospack = rospkg.RosPack()
+        # with open(f"{rospack.get_path('vehicle')}/map.bin", "wb") as f:
+        #     rospy.loginfo_throttle(1, "MAP: " + str(buff.getbuffer()))
+        #     f.write(buff.getbuffer())
+
     def obstacles_update_callback(self, data):
         if self.map is None:
             # wait for initial obstacles map
@@ -193,10 +225,13 @@ class Vehicle:
 
     def odom_callback(self, data):
         self.velocity = data.twist.twist.linear.x
+        self.yaw = get_rotation(data.pose.pose.orientation)
         self.pose = data.pose
         self.map_frame = data.header.frame_id
         self.car_frame = data.child_frame_id
         self.car_stamp = data.header.stamp
+        # self.L = 0.75 * self.velocity + 0.5 if self.velocity > sys.float_info.min else self.L
+        self.L = 1.5 * self.velocity if self.velocity > sys.float_info.min else self.L
 
         if self.map is None or self.goal is None:
             return
@@ -209,6 +244,24 @@ class Vehicle:
             if self.map_matrix[goal_in_map[0], goal_in_map[1]] >= 65:
                 # occupied!
                 self.obstacle_detected = True
+        # else:
+        #     theta = np.arange(self.steering_angle - math.radians(100), self.steering_angle + math.radians(100), math.radians(5), dtype=np.float32)
+        #     r = np.empty(theta.shape)
+        #     L = 1.5 * self.velocity if self.velocity > sys.float_info.min else self.L
+        #     r.fill(self.L)
+        #     x = np.empty(theta.shape)
+        #     x.fill(self.pose.pose.position.x)
+        #     y = np.empty(theta.shape)
+        #     y.fill(self.pose.pose.position.y)
+        #     d_x = x + r * np.cos(theta)
+        #     d_y = y + r * np.cos(theta)
+        #     projected_pos = np.column_stack((d_x, d_y))
+        #     pos_to_cell = lambda p: self.convert_position_to_grid_cell(p[1], p[0])
+        #     v_pos_to_cell = np.vectorize(pos_to_cell)
+        #     # projected_cells = v_pos_to_cell(projected_pos)
+        #     projected_cells = np.apply_along_axis(pos_to_cell, 0, projected_pos)
+        #     if not np.any(self.map_matrix[projected_cells[:, 0], projected_cells[:, 1]] > 65):
+        #         self.obstacle_detected = False
 
     def lidar_callback(self, data):
         if self.map is None or self.map.info.width == 0:
@@ -225,10 +278,26 @@ class Vehicle:
             projected_speed_array = self.velocity * np.cos(scan_angles)
             projected_speed_array[projected_speed_array < 0.1] = 0.1
             self.ttc_array = (np.maximum(0,scan_ranges - 0.2)) / projected_speed_array
-            angle = np.clip(self.steering_angle, -0.4189, 0.4189)
+            # angle = np.clip(self.steering_angle, -0.4189, 0.4189)
+            angle = np.clip(self.yaw, -0.4189, 0.4189)
             self.ttc = np.amin(self.ttc_array[self.myScanIndex(data, math.degrees(angle) - 45):self.myScanIndex(data, math.degrees(angle) + 45)])
-            if self.ttc > 2:
+            # self.ttc = np.amin(self.ttc_array[self.myScanIndex(data, -45):self.myScanIndex(data, 45)])
+            rospy.loginfo("ttc: " + str(self.ttc))
+            
+            # still_obstacle = False
+            # goal_in_map = self.convert_position_to_grid_cell(self.goal[1], self.goal[0])
+            # goal = self.convert_grid_cell_to_position(goal_in_map[1], goal_in_map[0])
+            # self.visualize_obstacle(goal[0], goal[1])
+            # # rospy.loginfo("OCCUPANCY VALUE FOR L: " + str(self.map_matrix[goal_in_map[0], goal_in_map[1]]))
+            # if self.map_matrix[goal_in_map[0], goal_in_map[1]] >= 65:
+            #     still_obstacle = True
+            # if self.ttc > 1.5 and not still_obstacle:
+            if self.ttc > 2.:
                 self.obstacle_detected = False
+            # ranges = np.array(data.ranges)
+            # front = ranges[self.myScanIndex(data, -125):self.myScanIndex(data, 125)]
+            # if not np.any(front < 2):
+            #     self.obstacle_detected = False
 
     def visualize_obstacle(self,x,y,frame='map',r=0.0,g=1.0,b=0.0):
         marker = Marker()
@@ -268,21 +337,14 @@ class Vehicle:
     
     def path_callback(self, data):
         self.path = data
-        # find goal point on path at lookahead_distance L
-        dist = 0
-        i = self.pp_start_index.data + 1
-        poses = np.array([(p.pose.position.x, p.pose.position.y) for p in self.path.poses])
-        ground_pos = self.path.poses[self.pp_start_index.data]
-        ground_pose = (ground_pos.pose.position.x, ground_pos.pose.position.y)
-        while i < len(poses):
-            dist = np.linalg.norm(poses[i] - ground_pose)
-            if dist > self.L:
-                break
-            i += 1
-            # Start a new lap
-            if self.multilap and i == len(poses):
-                i = 0
-        self.goal = poses[i - 1]    
+    
+        # buff = BytesIO()
+        # data.serialize(buff)
+        # # serialized_bytes = buff.getvalue()
+
+        # rospack = rospkg.RosPack()
+        # with open(f"{rospack.get_path('vehicle')}/path.bin", "wb") as f:
+        #     f.write(buff.getbuffer())
 
 def main(args):
     rospy.init_node("vehicle_node", anonymous=True)
@@ -291,7 +353,17 @@ def main(args):
     rfgs.map = rospy.wait_for_message('/costmap_node/costmap/costmap', OccupancyGrid)
     rfgs.map_matrix = np.array(rfgs.map.data).reshape((rfgs.map.info.height, rfgs.map.info.width))
     rospy.sleep(0.1)
+    rospack = rospkg.RosPack()
+    # with open(f"{rospack.get_path('vehicle')}/map.bin", "rb") as f:
+    #     buf = BytesIO(f.read())
+    #     rfgs.map.deserialize(buf.getvalue())
+    #     rfgs.map_matrix = np.matrix(preprocess_map(rfgs.map))
+    #     # rfgs.set_likelihood_field(rfgs.compute_likelihood_field())
+    #     rospy.sleep(0.1)
     rospy.spin()  
+
+    # with open(f"{rospack.get_path('vehicle')}/src/map.bin", "wb") as f:
+        # rfgs.map_file = f
 
 if __name__ == '__main__':
     main(sys.argv)
